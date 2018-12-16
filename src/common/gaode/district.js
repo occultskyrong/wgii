12342470c7a2aa3b69ea54db9988d8c4292b160e
occultskyrong <occultskyrong@163.com>
2018-12-16 19:25:56 +0800

    feat(data): update china GeoJSON data

--- src/common/gaode/district.js
+++ src/common/gaode/district.js
@@ -81,30 +81,37 @@ async function sparse() {
       properties: { name: 'China', code: 'CHN' },
     }],
   };
+  const MAX_HEIGHT = 1;
   const resultsCoordinates1 = []; // 结果集1
   const resultsCoordinates2 = []; // 结果集2
-  const {
-    features: [
-      [{ geometry: { coordinates } }],
-    ],
-  } = require('../../../dist/CHN/country.wgs84.geo.json');
+  const { features: [{ geometry: { coordinates } }] } = require('../../../dist/CHN/country.wgs84.geo.json');
   const pointsLength = coordinates.reduce((num, item) => num + item.length, 0);
   console.info('--- 共计 ', coordinates.length, ' 组 ， ', pointsLength, ' 个点数据 ---');
-  coordinates.filter(item => item.length > 100).forEach((pointsArray) => { // 过滤100个点以上的块
-    resultsCoordinates2.push([pointsArray]);
-    const results = douglasPeucker(pointsArray, 1);
-    if (results.length > 10) { // 结果中含10个点以上
-      resultsCoordinates1.push([results]);
-    }
-  });
+  coordinates.sort((a1, a2) => a2[0].length - a1[0].length)
+    // .forEach(item => console.info(item[0].length))
+    // .slice(0, 1)
+    // coordinates.forEach(item => console.info(item[0].length));
+    .filter(item => item[0].length > 100) // 过滤100个点以上的块
+    .forEach((item) => {
+      const pointsArray = item[0];
+      console.info(' -- 抽稀前点数量：', pointsArray.length);
+      // resultsCoordinates2.push([pointsArray]);
+      const results = douglasPeucker(pointsArray, MAX_HEIGHT);
+      // const results = douglasPeucker(pointsArray.slice(0, Math.ceil(pointsArray.length / 2)), 1);
+      console.info(' -- 抽稀后点数量：', results.length, ' --');
+      if (results.length > 10) { // 结果中含10个点以上
+        resultsCoordinates1.push([results]);
+      }
+    });
+  console.info('抽稀结果:共', resultsCoordinates1.length, '组数据');
   result.features[0].geometry.coordinates = resultsCoordinates1;
-  await fs.writeFileSync(path.join(__dirname, '../../../dist/CHN/country.wgs84.sparse.geo.json'), JSON.stringify(result));
-  result.features[0].geometry.coordinates = resultsCoordinates2;
-  await fs.writeFileSync(path.join(__dirname, '../../../dist/CHN/country.wgs84.sparse.geo.json'), JSON.stringify(result));
+  // await fs.writeFileSync(path.join(__dirname, '../../../dist/CHN/country.wgs84.sparse.geo.json'), JSON.stringify(result));
+  // result.features[0].geometry.coordinates = resultsCoordinates2;
+  await fs.writeFileSync(path.join(__dirname, `../../../dist/CHN/country.wgs84.${MAX_HEIGHT}.sparse.geo.json`), JSON.stringify(result));
 }
 
 async function run() {
-  await china();
+  await sparse();
 }
 
 run()
--- src/common/map/douglas-peucker.js
+++ src/common/map/douglas-peucker.js
@@ -7,6 +7,8 @@
  * 3、比较该距离与预先给定的阈值threshold的大小，如果小于threshold，则该直线段作为曲线的近似，该段曲线处理完毕。
  * 4、如果距离大于阈值，则用C将曲线分为两段AC和BC，并分别对两段取信进行1~3的处理。
  * 5、当所有曲线都处理完毕时，依次连接各个分割点形成的折线，即可以作为曲线的近似。
+ * @see https://www.jianshu.com/p/2e8e7b6562ba
+ * @see https://github.com/LiuTangLei/Douglas-Peucker-js/blob/master/douglas.js
  */
 
 const {
@@ -61,11 +63,14 @@ const calcHeight = (pointA, pointB, poingX) => {
  * @param {array} coordinates 坐标点
  * @param {int} max 阈值，单位km
  */
-const douglasPeucker = (coordinates, max) => {
+const sparse = (coordinates, max) => {
   let result = [];
+  if (!coordinates || coordinates.length <= 2) {
+    return result;
+  }
   const end = coordinates.length - 1; // 终点位置
-  let maxHeight = 0;
-  let maxIndex = 0;
+  let maxHeight = 0; // 最大弦高度
+  let maxIndex = 0; // 最大弦高度对应索引值
   const startPoint = coordinates[0]; // 起点
   const endPoint = coordinates[end]; // 终点
   // 依次计算起点、终点与其他点的弦高度，取最大值
@@ -76,16 +81,36 @@ const douglasPeucker = (coordinates, max) => {
       maxIndex = i;
     }
   }
-  if (maxHeight >= max) { // 若有点弦高超出阈值
+  if (maxHeight >= max) { // 若有点弦高超出阈值，则保留最大值点
     // 以最大点抽稀两侧散点
-    const leftResults = douglasPeucker(coordinates.slice(0, maxIndex), max);
-    const rightResults = douglasPeucker(coordinates.slice(maxIndex, end), max);
-    rightResults.slice(1, rightResults.length - 1); // 去除第一个点，重复
-    result = leftResults.concat(rightResults);
-  } else { // 不存在，说明此段散点构成线，较为平滑，取起始点即可
-    result.push(coordinates[0]);
-    result.push(coordinates[end]);
+    const leftResults = sparse(coordinates.slice(0, maxIndex), max);
+    const rightResults = sparse(coordinates.slice(maxIndex, end), max);
+    result = result.concat(leftResults);
+    result.push(coordinates[maxIndex]);
+    result = result.concat(rightResults);
   }
+  return result;
+};
+
+/**
+ * 抛出调用
+ * @param {array} coordinates 坐标点集
+ * @param {number} max 阈值
+ */
+const douglasPeucker = (coordinates, max) => {
+  let pointsArray = coordinates;
+  let result = [];
+  const { length } = coordinates;
+  if (!coordinates || length <= 2) {
+    return result;
+  }
+  // 闭环曲线，首尾相同，去除尾点
+  if (JSON.stringify(coordinates[0]) === JSON.stringify(coordinates[length - 1])) {
+    pointsArray = coordinates.slice(0, length - 2);
+  }
+  result.push(pointsArray[0]);
+  result = result.concat(sparse(pointsArray, max));
+  result.push(coordinates[length - 1]);
   return result;
 };
 
