fcf91feca52e7aaf4100160bdbc7668f1c6e8400
occultskyrong <occultskyrong@163.com>
2019-01-30 10:58:44 +0800

    feat(code): update data code

--- src/_scripts/_init.js
+++ src/_scripts/_init.js
@@ -28,16 +28,33 @@ countries.forEach((country) => {
   const { country_code3: countryCode3, country_name_english_UN: name } = country;
   if (countryCode3 in geoJsonCodePath) {
     const geoJsonFilepath = geoJsonCodePath[countryCode3];
-    const distFilePath = path.join(distDirpath, countryCode3, 'country.bd09.geo.json');
-    const { features: [{ geometry: { coordinates } }] } = require(geoJsonFilepath);
+    const distFilePath = path.join(distDirpath, countryCode3, 'country.wgs84.geo.json');
+    const resourceFilePath = path.join(distDirpath, countryCode3, 'country.resource.geo.json');
+    // const delFilePath = path.join(distDirpath, countryCode3, 'country.bd09.geo.json');
+    // fs.unlinkSync(delFilePath); // 删除 bd09 原文件
+    const resource = require(geoJsonFilepath);
+    const { features: [{ geometry: { coordinates } }] } = resource;
     const geojson = {
       type: 'FeatureCollection',
       features: [{
         type: 'Feature',
+        id: countryCode3,
         geometry: { type: 'MultiPolygon', coordinates: [coordinates] },
-        properties: { name, code: countryCode3, coordinatesSystem: 'BD09' },
+        properties: { name, code: countryCode3, coordinatesSystem: 'WGS84' },
       }],
     };
     fs.writeFileSync(distFilePath, JSON.stringify(geojson));
+    fs.writeFileSync(resourceFilePath, JSON.stringify(resource));
   }
 });
+
+// 获取缺少数据的国家
+// const noSuchCountriesData = [];
+// countries.forEach((country) => {
+//   const { country_code3: countryCode3, country_name_english_UN: name, country_name_chinese_UN: chineseName } = country;
+//   if (!(countryCode3 in geoJsonCodePath)) {
+//     noSuchCountriesData.push({ code3: countryCode3, chineseName, name });
+//   }
+// });
+
+// console.info(noSuchCountriesData);
